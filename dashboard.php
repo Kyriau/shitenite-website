0f9348edc8eab51e8e6462d726a3dc91b1add188
Kyriau <jeff.aj.thomson@gmail.com>
2021-06-10 08:39:27 -0700

    new page for user dashboard

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<?php
+	
+	include "site-init.php";
+	
+?>
+
+<!DOCTYPE html>
+<html lang=en>
+	<head>
+	
+		<title>Shite Nite</title>
+		<link rel="icon" href="img/favicon.png">
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
+	</head>
+	<body>
+		<div class="main-content">
+			<?php include "site-header.php"; ?>
+			<div class="center-container">
+				<h2>Gallery</h2>
+				<h3>View a gallery of movies currently available in the pile, and make nominations for voting.</h3>
+			</div>
+		</div>
+	</body>
+</html>
